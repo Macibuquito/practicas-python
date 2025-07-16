398f808454498e75b540bc10b5527f4707831fc0
Macibuquito <carlosevargasfernadez@gmail.com>
2025-07-16 13:21:08 +0200

    Pilar - Abstracción

--- practica2.py
+++ practica2.py
@@ -1,2 +1,53 @@
 # Practica 2
-print("Hola, esta es la práctica 2 en GitHub con Carlitos ❤️")
+# print("Hola, esta es la práctica 2 en GitHub con Carlitos ❤️")
+
+# Los cuatro pilares fundamentales de la POO:
+   # aplicando los principios de:
+       # 1. Abstracción  --> Solo muestras detalles escensiales de un objeto OCULTA complejidad interna
+           # Basta con saber como acelerar, frenar y encender el coche, NO conocer su funcionamiento interno
+
+print("Ej. Abstracción")
+class Cafetera:
+    def encender(self):
+        print("Cafetera encendida")
+
+    def preparar_cafe(self):
+        print("Preparando cafe")
+
+    def apagar(self):
+        print("Cafetera apagada")
+
+cafetera = Cafetera()
+cafetera.preparar_cafe()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
